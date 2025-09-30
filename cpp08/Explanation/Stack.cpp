4b1fd77371c6f156bcb2cc6a599fbf9a90b396a5
Lubna Khazaaleh <lkhazaal@c1r4s13.42amman.com>
2025-09-30 16:24:01 +0300

    doing cpp08/ex02

--- cpp08/Explanation/Stack.cpp
+++ cpp08/Explanation/Stack.cpp
@@ -2,7 +2,19 @@
 and deletion. It means that most recently inserted element is
 removed first and the first inserted element will be removed last.
 This is done by inserting and deleting elements at only one end of
-the stack which is generally called the top of the stack.*/
+the stack which is generally called the top of the stack.
+
+* std::stack is not a container itself, it is an adaptor.
+It uses another container underneath (like deque or vector) to actually store elements.
+By default, it uses deque.
+
+
+* Protected member c in stack:
+Internally, std::stack has a protected member called c.
+c is the actual container that holds the data.
+Since it is protected, classes that inherit from std::stack can access it.
+
+*/
 
 #include <iostream>
 #include <stack>
--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -2,14 +2,22 @@
 #define MUTANTSTACK_HPP
 
 #include <stack>
+#include <deque>
 #include <iostream>
 
-template <typename T>
-class MutantStack : public std::stack<T>
+template <typename T, typename Container = std::deque<T> >
+class MutantStack : public std::stack<T, Container>
 {
     private:
-        
+        int _size;
     public:
+        typedef typename Container::iterator iterator;
+        
+
+
+
+
+
 };
 
 #endif
